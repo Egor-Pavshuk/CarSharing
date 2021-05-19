5bfc8c28a8233786e6325b30931253667c8ac670
Егор Павшук <egor.pavshuk@outlook.com>
2021-05-19 15:08:05 +0300

    Added arrows to pager

--- WebCarSharing2/HtmlHelpers/PagingHelpers.cs
+++ WebCarSharing2/HtmlHelpers/PagingHelpers.cs
@@ -17,18 +17,48 @@ namespace WebCarSharing2.HtmlHelpers
         )
         {
             StringBuilder result = new StringBuilder();
-
-            TagBuilder arrowTag = new TagBuilder("a"); //todo or not
+            
+             
 
             for (int i = 1; i <= pagingInfo.TotalPages(); i++)
             {
-                TagBuilder tag = new TagBuilder("a"); //todo with div
+                if (pagingInfo.CurrentPage != 1 && i == 1)
+                {
+                    TagBuilder arrowTag = new TagBuilder("a");//todo or not
+                    TagBuilder arrowDiv = new TagBuilder("div");
+                    arrowDiv.AddCssClass("btn btn-default");
+                    arrowTag.MergeAttribute("href", "javascript: PageClick(" + i + ")");
+                    arrowDiv.InnerHtml = "<";
+
+                    arrowTag.InnerHtml += arrowDiv.ToString(TagRenderMode.Normal);
+                    result.Append(arrowTag);
+                }
+
+                TagBuilder div = new TagBuilder("div");
+                div.AddCssClass("btn btn-default");
+
+                TagBuilder tag = new TagBuilder("a"); 
                 tag.MergeAttribute("href","javascript: PageClick(" + i + ")");
-                tag.InnerHtml = i.ToString();
+
+                div.InnerHtml = i.ToString();
                 if(i == pagingInfo.CurrentPage)
-                    tag.AddCssClass("selected");
-                result.Append(tag);
+                    div.AddCssClass("selected");
                 
+                tag.InnerHtml += div.ToString(TagRenderMode.Normal);
+                result.Append(tag);
+
+
+                if (pagingInfo.CurrentPage != pagingInfo.TotalPages() && i == pagingInfo.TotalPages())
+                {
+                    TagBuilder arrowTag = new TagBuilder("a");//todo or not
+                    TagBuilder arrowDiv = new TagBuilder("div");
+                    arrowDiv.AddCssClass("btn btn-default");
+                    arrowTag.MergeAttribute("href", "javascript: PageClick(" + i + ")");
+                    arrowDiv.InnerHtml = ">";
+
+                    arrowTag.InnerHtml += arrowDiv.ToString(TagRenderMode.Normal);
+                    result.Append(arrowTag);
+                }
             }
             return MvcHtmlString.Create(result.ToString());
         }
